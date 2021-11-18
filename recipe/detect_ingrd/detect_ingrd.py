9a88e198922b4b89e8c4752c67a6dc89f5306ce2
unknown <dobbie6174@gmail.com>
2021-11-18 13:30:16 +0900

    ml result format change

--- recipe/detect_ingrd/detect_ingrd.py
+++ recipe/detect_ingrd/detect_ingrd.py
@@ -23,17 +23,17 @@ class Dectect_Ingrd:
 
         self.model = torch.hub.load('ultralytics/yolov5', 'custom', path=pt)
 
-    def detect(self, img_path): #return result, json_result
+    def detect(self, img_path): #return result, np_result
         img = Image.open(img_path)
         result = self.model(img)
-        # index / xmin / ymin / xmax / ymax / confidence / class / name
-        return result, result.pandas().xyxy[0].to_json(orient="records")
+        return result, result.pandas().xyxy[0].to_numpy()
 
 if __name__ == "__main__":
     detection = Dectect_Ingrd() #Dectect_Ingrd('best')
-    result, json_result = detection.detect("E:/test.jpg") #for test
+    result, np_result = detection.detect("E:/test.jpg") #for test
 
     # result.print()
     # result.save()
-    print(json_result) #to json format
-    print(json_result[0].name)
+
+    # xmin / ymin / xmax / ymax / confidence / class / name
+    print(np_result[0][-1]) #numpy format
