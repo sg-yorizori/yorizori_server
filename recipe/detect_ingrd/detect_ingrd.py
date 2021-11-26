0e526273b801cc87d80fc9c1549cf244de2c0427
unknown <dobbie6174@gmail.com>
2021-11-26 22:10:05 +0900

    add detected ingrd list method

--- recipe/detect_ingrd/detect_ingrd.py
+++ recipe/detect_ingrd/detect_ingrd.py
@@ -1,4 +1,5 @@
 import os
+import os.path as osp
 import torch
 from PIL import Image
 from download_pt import download_file_from_google_drive
@@ -23,17 +24,37 @@ class Dectect_Ingrd:
 
         self.model = torch.hub.load('ultralytics/yolov5', 'custom', path=pt)
 
-    def detect(self, img_path): #return result, np_result
-        img = Image.open(img_path)
-        result = self.model(img)
-        return result, result.pandas().xyxy[0].to_numpy()
+    def detect(self, input_img_path, dest_img_path='result'): #return np_result, img_result_path
+        img = Image.open(input_img_path)
+
+        self.result = self.model(img)
+        self.np_result = self.result.pandas().xyxy[0].to_numpy()
+        self.ingrd_list = [self.np_result[i][-1] for i in range(len(self.np_result))]
+
+        # save img in dest_img_path/test.jpg
+        self.result.save(dest_img_path)
+        dest_img = osp.join(os.getcwd(),osp.join(dest_img_path, 'test.jpg')) #절대 경로
+
+        return self.np_result, dest_img
+    
+    def get_result_ingrd_list(self):
+        return self.ingrd_list
+
+    def get_xyxy_ingrd(self, ingrd):
+        if ingrd in self.ingrd_list:
+            idx = self.ingrd_list.index(ingrd)
+            return True, self.np_result[idx][:4]
+        else:
+            return False, "no such ingrd"
 
 if __name__ == "__main__":
     detection = Dectect_Ingrd() #Dectect_Ingrd('best')
-    result, np_result = detection.detect("E:/test.jpg") #for test
-
-    # result.print()
-    # result.save()
+    np_result, img_result = detection.detect("E:/test.jpg") #for test
 
     # xmin / ymin / xmax / ymax / confidence / class / name
-    print(np_result[0][-1]) #numpy format
+    print(np_result)
+    print(img_result)
+    print(detection.get_result_ingrd_list())
+    
+    _, xyxy = detection.get_xyxy_ingrd('paprika')
+    print(xyxy)
